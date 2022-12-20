9ce3d36edb6e5e2d2ef50f36b72936c5020a905e
kejiahp <popoolakejiah@gmail.com>
2022-12-20 22:21:14 +0100

    deleted build folder

--- build/controller/homepage-controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getHomepage = void 0;
-const getHomepage = (req, res) => {
-    res.render('homepage.ejs');
-};
-exports.getHomepage = getHomepage;
