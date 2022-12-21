12f852bdd09a89dcf20a7e18c1d3296f4529214d
kejiahp <popoolakejiah@gmail.com>
2022-12-21 10:42:32 +0100

    webhook controller fixed

--- src/controller/webhook-controller.ts
+++ src/controller/webhook-controller.ts
@@ -29,17 +29,14 @@ export const getWebHook = (req: Request, res: Response) => {
 
 export const postWebHook = (req: Request, res: Response) => {
     const body = req.body
-    console.log("This is the request body",req.body)
     if (body.object === 'page') {
       
         body.entry.forEach((entry:any) => {
     
           let webhook_event = entry.messaging[0];
-          console.log(webhook_event);
   
           //get the sender psid - page scoped id
           let sender_psid = webhook_event.sender.id
-          console.log('Sender PSID: '+sender_psid)
           
           if (webhook_event.message) {
             handleMessage(sender_psid, webhook_event.message)
